56ee148e163e244d7e4abf194067b3d1896f8712
Lam Tran <tranvulam.dev@gmail.com>
2022-09-06 09:02:26 +0700

    first commit: docker - prisma

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+
+@Injectable({})
+export class AuthService {
+  signin() {
+    return { msg: 'signin' };
+  }
+
+  signup() {
+    return { msg: 'signin' };
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3300, () => console.log('App listening on port 3300'));
+}
+bootstrap();
